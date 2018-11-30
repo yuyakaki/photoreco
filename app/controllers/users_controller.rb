23946f2df7f6ec76b667a6382b5adcf82e181d9c
yuuya.kakizono <yuya07sid@hotmail.com>
2018-11-30 09:41:53 +0000

    requre_user_logged_in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :edit]
+  
   def index
     @users = User.all.page(params[:page])
   end
